8b3887a0c210d3e91b66bc0c24b8c6d30b3edb2b
Husamdababneh <d_husam@yahoo.com>
2020-02-18 21:54:33 +0200

    Updated premake script & added gitignore file

--- premake5.lua
+++ premake5.lua
@@ -20,7 +20,6 @@ project "GLFW"
     
 	filter "system:windows"
         buildoptions { "-std=c11", "-lgdi32" }
-        systemversion "10.0.17134.0"
         staticruntime "On"
         
         files
